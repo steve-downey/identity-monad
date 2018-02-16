8243b698064c43be3c4ea7507c902b0d5deb4b12
Steve Downey <sdowney@gmail.com>
2018-02-15 23:08:47 -0500

    Reformat

--- lib/identity.h
+++ lib/identity.h
@@ -25,26 +25,21 @@ class Identity {
     Identity(T const& t) : t_(t){};
 
     template <typename U, typename Func>
-    friend
-    auto fmap(Identity<U> const& i, Func const& f)
+    friend auto fmap(Identity<U> const& i, Func const& f)
         -> Identity<decltype(f(i.t_))>;
 
     template <typename U, typename Func>
-    friend
-    auto bind(Identity<U> const& i, Func const& f)
+    friend auto bind(Identity<U> const& i, Func const& f)
         -> std::invoke_result_t<Func, U>;
 
     template <typename U>
-    friend
-    auto join(Identity<Identity<U>> i) -> Identity<U>;
+    friend auto join(Identity<Identity<U>> i) -> Identity<U>;
 
     template <typename U, typename V>
-    friend
-    bool operator==(Identity<U> u, Identity<V> v);
+    friend bool operator==(Identity<U> u, Identity<V> v);
 
     template <typename U, typename V>
-    friend
-    bool operator!=(Identity<U> t, Identity<V> u);
+    friend bool operator!=(Identity<U> t, Identity<V> u);
 };
 
 // ============================================================================
@@ -52,19 +47,16 @@ class Identity {
 // ============================================================================
 
 template <typename T, typename Func>
-auto fmap(Identity<T> const& i, Func const& f)
-    -> Identity<decltype(f(i.t_))>
-{
+auto fmap(Identity<T> const& i, Func const& f) -> Identity<decltype(f(i.t_))> {
     using U = std::invoke_result_t<Func, T>;
     // f is of type T -> U
     return Identity<U>{std::invoke(f, i.t_)};
 }
 
 template <typename Func>
-auto fmap(Func const& f)
-{
+auto fmap(Func const& f) {
     // maps (T -> U) -> (Identity<T> -> Identity<U>)
-    return [=](auto t){return fmap(t, f);};
+    return [=](auto t) { return fmap(t, f); };
 }
 
 template <typename T>
@@ -75,11 +67,9 @@ struct template_parameter<Identity<T>> {
     typedef T type;
 };
 
-
 template <typename T, typename Func>
 auto bind(Identity<T> const& i, Func const& f)
-    -> std::invoke_result_t<Func, T>
-{
+    -> std::invoke_result_t<Func, T> {
     using M = std::invoke_result_t<Func, T>;
     using U = typename template_parameter<M>::type;
     // assert that f is of type T -> Identity<U>
@@ -89,13 +79,11 @@ auto bind(Identity<T> const& i, Func const& f)
 }
 
 template <typename Func>
-auto bind(Func const& f)
-{
+auto bind(Func const& f) {
     // lifts the argument into the monad and joins the result:
     // (T -> Identity<U>) -> (Identity<T> -> Identity<U>)
-    return [=](auto t){return bind(t, f);};
+    return [=](auto t) { return bind(t, f); };
 }
-
 
 template <typename T>
 Identity<T> make(T t) {
@@ -103,8 +91,7 @@ Identity<T> make(T t) {
 }
 
 template <typename T>
-auto join(Identity<Identity<T>> i) -> Identity<T>
-{
+auto join(Identity<Identity<T>> i) -> Identity<T> {
     return Identity<T>{i.t_};
 }
 
@@ -120,7 +107,7 @@ bool operator!=(Identity<T> t, Identity<U> u) {
 
 template <typename Func>
 auto curry(Func f) {
-    if constexpr (std::is_invocable<Func>::value){
+    if constexpr (std::is_invocable<Func>::value) {
         return f();
     } else {
         return [f](auto&& x) {
